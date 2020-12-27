567a6d1241829501cb54cd4b8fc58f63be2d65ce
d.boldin <d.boldin@corp.mail.ru>
2020-12-28 01:11:22 +0300

    feedback added to admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from users.models import User, EgeResults, Favorite
+from users.models import User, EgeResults, Favorite, Feedback
 
 admin.site.register(User)
 admin.site.register(EgeResults)
 admin.site.register(Favorite)
+admin.site.register(Feedback)
