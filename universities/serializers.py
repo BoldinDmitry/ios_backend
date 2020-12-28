1d252d7b0eba23d8b0e6123addc7bd06d3ec9f6a
d.boldin <d.boldin@corp.mail.ru>
2020-12-29 01:00:24 +0300

    photos from university model deleted

--- universities/serializers.py
+++ universities/serializers.py
@@ -28,13 +28,13 @@ class EdProgramSerializer(serializers.ModelSerializer):
 
 
 class UniversitySerializer(serializers.ModelSerializer):
-    photos = serializers.SerializerMethodField('get_photos', read_only=True)
+    # photos = serializers.SerializerMethodField('get_photos', read_only=True)
     ed_programs = serializers.SerializerMethodField('get_ed_programs', read_only=True)
 
-    @staticmethod
-    def get_photos(university):
-        photos = university.get_photos()
-        return UniversityPhotosSerializer(photos, many=True).data
+    # @staticmethod
+    # def get_photos(university):
+    #     photos = university.get_photos()
+    #     return UniversityPhotosSerializer(photos, many=True).data
 
     @staticmethod
     def get_ed_programs(university):
@@ -44,7 +44,7 @@ class UniversitySerializer(serializers.ModelSerializer):
     class Meta:
         model = University
         fields = ['id',
-                  'photos',
+                  # 'photos',
                   'ed_programs',
                   'name',
                   'about',
